c5aa8d3eeeba75c0c48412c7476ab7aee745c538
Oleg Vidyaev <ovidyaev@setka.io>
2018-02-22 17:43:08 +0300

    fix pool config

--- app/services/pool_config.rb
+++ app/services/pool_config.rb
@@ -11,65 +11,7 @@ class PoolConfig
       PoolConfig::Bth.as_json,
       PoolConfig::Dash.as_json,
       PoolConfig::Ltc.as_json,
-      PoolConfig::Dgb.as_json
+      PoolConfig::Dgb.as_json,
     ]
   end
-
-  def initialize(options={})
-    @id= options["id"]
-    @currency = options["currency"]
-    @address = options["address"]
-  end
-
-  def miners
-    if id == 'ltc1'
-      ['L3']
-    elsif id == 'btc2x1'
-      ['D3']
-    elsif id == 'dgb-sha256-1'
-      ['M3', 'S9']
-    elsif id == 'dgb_scrypt'
-      ['L3']
-    else
-      []
-    end
-  end
-
-  def label
-    if id == 'ltc1'
-      'Litecoin'
-    elsif id == 'btc2x1'
-      'Segwit2x'
-    elsif id == 'dgb-sha256-1'
-      'DGB sha256'
-    elsif id == 'dgb_scrypt'
-      'DGB scrypt'
-    else
-      ''
-    end
-  end
-
-  def settings
-    if id == 'ltc1'
-      PoolConfig::Ltc.config
-    elsif id == 'btc2x1'
-      PoolConfig::B2x.config
-    elsif id == 'dgb-sha256-1'
-      PoolConfig::Dgb_sha256.config
-    elsif id == 'dgb_scrypt'
-      PoolConfig::Dgb_scrypt.config
-    else
-      {}
-    end
-  end
-
-  def as_json
-    {
-      id: id,
-      miners: miners,
-      label: label,
-      settings: settings
-    }
-  end
-
 end
