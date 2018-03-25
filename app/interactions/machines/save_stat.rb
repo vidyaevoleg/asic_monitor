b65ff16f2b48f8c69d4d7ea124880fe3b8018882
Oleg Vidyaev <ovidyaev@setka.io>
2018-03-25 23:50:24 +0300

    fix

--- app/interactions/machines/save_stat.rb
+++ app/interactions/machines/save_stat.rb
@@ -17,9 +17,11 @@ module Machines
     private
 
     def save_actual_stat
-      last_actual_stat = machine.stats.order(id: :asc).where(success: true).last
-      if last_actual_stat && last_actual_stat&.blocks.to_i > 0 && info[:blocks] == "0"
-        machine.update(blocks_count: machine.blocks_count + last_actual_stat&.blocks.to_i)
+      last_actual_stat = machine.stats.order(id: :desc).where(success: true).limit(1).first
+      if info[:blocks].to_i > last_actual_stat&.blocks.to_i
+        # we found new block
+        diff = info[:blocks].to_i - last_actual_stat.blocks
+        machine.update(blocks_count: machine.blocks_count + diff)
       end
       stat = machine.stats.create(info)
       StatAnalyzer.call(stat)
