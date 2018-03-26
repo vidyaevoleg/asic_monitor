c691108ecd0202d041ccbfb6b5e18fc459cbfdfe
Oleg Vidyaev <ovidyaev@setka.io>
2018-03-26 20:34:06 +0300

    fix

--- app/interactions/machines/save_stat.rb
+++ app/interactions/machines/save_stat.rb
@@ -17,7 +17,7 @@ module Machines
     private
 
     def save_actual_stat
-      last_actual_stat = machine.stats.order(id: :desc).where(success: true).limit(1).first
+      last_actual_stat = machine.stats.order(id: :desc).where(success: true, active: true).limit(1).first
       if info[:blocks].to_i > last_actual_stat&.blocks.to_i
         # we found new block
         diff = info[:blocks].to_i - last_actual_stat.blocks
