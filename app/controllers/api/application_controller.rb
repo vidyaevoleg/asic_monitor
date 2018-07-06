38601f7ad9962030b327044e9ff947cacd37b09f
Oleg Vidyaev <ovidyaev@setka.io>
2018-07-06 15:40:37 +0300

    fix typo

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -1,7 +1,6 @@
 module Api
   class ApplicationController < ::ApplicationController
     self.responder = ::ApplicationResponder
-    skip_before_action :verify_authenticity_token
     respond_to :json
   end
 end
