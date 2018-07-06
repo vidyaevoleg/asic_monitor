b30fa56c866eee0b526450db0775bb4843057e69
Oleg Vidyaev <ovidyaev@setka.io>
2018-07-06 15:35:55 +0300

    typo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :process_params
   skip_before_action :verify_authenticity_token
+  before_action :authenticate_user!, unless: :hotel?
 
   private
 
